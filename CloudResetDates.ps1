138ca492165f0c94cd2e86794f3233319a419992
Austin Guidry <128058966+AustinGuidry@users.noreply.github.com>
2025-04-06 20:08:16 -0500

    Update CloudResetDates.ps1 (authentication mode)

--- CloudResetDates.ps1
+++ CloudResetDates.ps1
@@ -1,11 +1,8 @@
-Install-Module Microsoft.Graph -Scope CurrentUser -Force
-Install-Module ImportExcel -Scope CurrentUser -Force
-
 Import-Module Microsoft.Graph
 Import-Module ImportExcel
 
 Write-Host "Connecting to MS Graph..." -ForegroundColor Cyan
-Connect-MgGraph -Scopes "User.Read.All", "Directory.Read.All" -UseDeviceAuthentication
+Connect-MgGraph -Scopes "User.Read.All", "Directory.Read.All" -UseDeviceCode
 Write-Host "Connected to MS Graph." -ForegroundColor Green
 
 Write-Host "Retrieving user information..." -ForegroundColor Cyan
@@ -31,9 +28,9 @@ foreach ($user in $users) {
 
 Write-Host "Outputting to table..." -ForegroundColor Cyan
 
-$excelPath = "$env:USERPROFILE\Documents\CloudOnlyUsers_LastPasswordChange.xlsx"
-$allUsers | Sort-Object LastPasswordChangeDateTime | Export-Excel -Path $excelPath -WorksheetName "US Cloud Users" -AutoSize -BoldTopRow -FreezeTopRow
+$excelPath = "$env:USERPROFILE\Documents\clouduserlist.xlsx"
+$allUsers | Sort-Object LastPasswordChangeDateTime | Export-Excel -Path $excelPath -WorksheetName "US_Cloud_Users" -AutoSize -BoldTopRow -FreezeTopRow
 
-Write-Host "The data has been exported to $excelPath" -ForegroundColor Green
+Write-Host "The data has been successfully exported to $excelPath" -ForegroundColor Green
 
 Disconnect-MgGraph
